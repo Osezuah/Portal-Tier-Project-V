a578f1a890774d97f78464876c654d1f6a9a0ac7
Osezuah <tessosezuah@gmail.com>
2024-11-12 15:42:14 -0500

    assigned routes to team

--- PortalCommunications/PortalCommunications/Components/Endpoints CS/APIEndpoints.cs
+++ PortalCommunications/PortalCommunications/Components/Endpoints CS/APIEndpoints.cs
@@ -6,37 +6,24 @@ public class APIEndpoints
 {
     public static void Map(WebApplication app) 
     {
-        app.MapPut("/api/device-status/", async(JsonElement JSobject) =>
+        //route that accepts changes made to devices on Home
+        app.MapPut("/api/device-status/", async (JsonElement JSobject) =>
         {
-            JSobject.TryGetProperty("device-type", out JsonElement DT);
-            string Type = DT.GetString();
 
-            return Results.Ok(new {message = "Device Type is:", Type});
-
-            //BUsiness layer's commands - Give them Objec
-           // return Results.Ok(JSobject);
-        })
-        .WithName("ReceiveDeviceStatus")
-        .WithTags("Device");
-       
-        //app.MapGet("/", async context =>
-        //{
-        //    //A get Action
-        //    await context.Response.WriteAsJsonAsync(new { Message = "All todo items." });
-        //});
+        });
 
         //This will be used to get a specific device
         app.MapGet("/api/{id}", async context =>
         {
-            // Get one todo item
-
-            await context.Response.WriteAsJsonAsync(new { Message = "One todo item" });
+            
         });
-        //Create a route that will authenticate user login credentials.
-        //Go to line 33 in Login.razor to chaange the formaction link
 
+        //Create a route that will authenticate user login credentials - Go to line 33 in Login.razor to chaange the formaction link Thomas
 
+        //Create a route that sends changes user makes to device in the ui to home application Brian
+        
+        //Create a route that registers devices Rian
+
+        //create a route that registers a new user Ehi
     }
 }
-
-public record DeviceStatus(String device_name, String location, String status, double? temperature);
