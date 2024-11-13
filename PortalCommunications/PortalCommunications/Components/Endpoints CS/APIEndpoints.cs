b2e93061860a28b2abff48476ffb0bd229de25be
Askoot94 <98133009+Askoot94@users.noreply.github.com>
2024-11-13 14:12:40 -0500

    Created register-devices endpoint

--- PortalCommunications/PortalCommunications/Components/Endpoints CS/APIEndpoints.cs
+++ PortalCommunications/PortalCommunications/Components/Endpoints CS/APIEndpoints.cs
@@ -1,10 +1,12 @@
-﻿using System.Runtime.InteropServices;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
 using System.Text.Json;
 namespace APISeperateFiles;
 
 public class APIEndpoints
 {
-    public static void Map(WebApplication app) 
+    public static void Map(WebApplication app)
     {
         //route that accepts changes made to devices on Home
         app.MapPut("/api/device-status/", async (JsonElement JSobject) =>
@@ -15,7 +17,7 @@ public class APIEndpoints
         //This will be used to get a specific device
         app.MapGet("/api/{id}", async (JsonElement JSobject) =>
         {
-            
+
         });
 
         //Create a route that will authenticate user login credentials - Go to line 33 in Login.razor to chaange the formaction link Thomas
@@ -26,7 +28,13 @@ public class APIEndpoints
 
         });
 
-        //Create a route that registers devices Rian
+        //Create a route that registers devices
+        //Expecting a single device within the passed JSON Element.
+        app.MapPost("/api/register-devices/", async (JsonDocument payload) =>
+        {
+            
+        });
+
 
         //create a route that registers a new user
         app.MapPost("/api/register-user/", async (JsonElement JSobject) =>
