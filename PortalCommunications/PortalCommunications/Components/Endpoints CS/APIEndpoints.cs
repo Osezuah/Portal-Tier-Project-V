b1e0462e3e7425b866328d1c7351979b03cc739d
Thomas heal <81531490+ThomasHeal@users.noreply.github.com>
2024-11-13 16:44:05 -0500

    Update APIEndpoints.cs
    - Added blank route

--- PortalCommunications/PortalCommunications/Components/Endpoints CS/APIEndpoints.cs
+++ PortalCommunications/PortalCommunications/Components/Endpoints CS/APIEndpoints.cs
@@ -21,6 +21,10 @@ public class APIEndpoints
         });
 
         ////Create a route that will authenticate user login credentials - Go to line 33 in Login.razor to chaange the formaction link Thomas
+        ///
+        app.MapGet("/api/user", async (JsonElement JSobject) =>
+        {
+        });
 
         ////Create a route that sends changes user makes to device in the ui to home application
         //app.MapPut("/api/device-changes/", async (JsonElement JSobject) =>
