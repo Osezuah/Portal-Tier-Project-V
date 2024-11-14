ded260503cda0db52189e09d3369040780dc5a24
Askoot94 <98133009+Askoot94@users.noreply.github.com>
2024-11-13 20:38:05 -0500

    Uncommented the Endpoints, all works

--- PortalCommunications/PortalCommunications/Components/Endpoints CS/APIEndpoints.cs
+++ PortalCommunications/PortalCommunications/Components/Endpoints CS/APIEndpoints.cs
@@ -9,10 +9,10 @@ public class APIEndpoints
     public static void Map(WebApplication app)
     {
         ////route that accepts changes made to devices on Home
-        //app.MapPut("/api/device-status/", async (JsonElement JSobject) =>
-        //{
+        app.MapPut("/api/device-status/", async (JsonElement JSobject) =>
+        {
 
-        //});
+        });
 
         //This will be used to get a specific device
         app.MapGet("/api/device/{int Idnumber}", async (Idnumber) =>
@@ -27,10 +27,10 @@ public class APIEndpoints
         });
 
         ////Create a route that sends changes user makes to device in the ui to home application
-        //app.MapPut("/api/device-changes/", async (JsonElement JSobject) =>
-        //{
+        app.MapPut("/api/device-changes/", async (JsonElement JSobject) =>
+        {
 
-        //});
+        });
 
         //Create a route that registers devices
         //Expecting a single device within the passed JSON Element.
@@ -60,9 +60,9 @@ public class APIEndpoints
 
 
         ////create a route that registers a new user
-        //app.MapPost("/api/register-user/", async (JsonElement JSobject) =>
-        //{
+        app.MapPost("/api/register-user/", async (JsonElement JSobject) =>
+        {
 
-        //});
+        });
     }
 }
