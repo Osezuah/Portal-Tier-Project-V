43f2f5506b71d8a87da28d777d6a2858e6197931
AnujMongia1 <amongia1308@conestogac.on.ca>
2024-11-26 15:59:38 -0500

    working!!!!

---
+++ Databases/Databases/Models/Room.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Databases.Models
+{
+    [PrimaryKey(nameof(Id))]
+    public class Room : PortalDeviceContext
+    {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [ForeignKey("Home")]
+        public int HomeId { get; set; }
+        
+        public Home ParentHome { get; set; }
+        public ICollection<Device> Devices { get; set; } = new List<Device>();
+    }
+}
+
---
+++ PortalCommunications/PortalCommunications/Components/Services/AccountService.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net.Http.Json;
+
+namespace PortalCommunications.Components.Services
+{
+    public class AccountService
+    {
+        private readonly HttpClient _http;
+
+        public AccountService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        // Method to delete a user account by ID
+        public async Task<bool> DeleteAccountAsync(int userId)
+        {
+            var response = await _http.DeleteAsync($"api/users/{userId}");
+            return response.IsSuccessStatusCode;
+        }
+
+    }
+}
