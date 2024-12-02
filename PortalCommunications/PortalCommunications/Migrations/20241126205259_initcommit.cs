28c6102a7a5d158d76e7c888446c7cab47c05e09
Osezuah <tessosezuah@gmail.com>
2024-12-02 12:25:42 -0500

    Naming convention -renamed initcommit from all lowercase to capitalized I

--- PortalCommunications/PortalCommunications/Migrations/20241126205259_initcommit.cs
+++ PortalCommunications/PortalCommunications/Migrations/20241126205259_initcommit.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace PortalCommunications.Migrations
 {
     /// <inheritdoc />
-    public partial class initcommit : Migration
+    public partial class Initcommit : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
