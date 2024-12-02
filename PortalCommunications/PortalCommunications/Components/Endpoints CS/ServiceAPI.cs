1d21e956ddfe8ce139a9fcc00d6cfbd74bdb1f49
Osezuah <tessosezuah@gmail.com>
2024-12-02 12:24:15 -0500

    Some async tasks conform to Models User class

--- PortalCommunications/PortalCommunications/Components/Endpoints CS/ServiceAPI.cs
+++ PortalCommunications/PortalCommunications/Components/Endpoints CS/ServiceAPI.cs
@@ -2,8 +2,9 @@
 using System.Runtime.CompilerServices;
 using Microsoft.Win32;
 using System.Threading.Tasks;
-using PortalCommunications.Components.Device_Class;
 using PortalCommunications.Components.Pages;
+using PortalCommunications.Models;
+
 
 public class ServiceAPI
 {
@@ -20,7 +21,7 @@ public class ServiceAPI
     public async Task<User> LoginTask (int id, string username, string password)
     {
         //create payload - User object
-        var loginPayload = new User {id = id, username = username, password = password}; 
+        var loginPayload = new User {Id = id, FirstName = username, Password = password}; 
 
         var response = await _httpClient.PostAsJsonAsync("/api/user", loginPayload);
 
@@ -30,9 +31,9 @@ public class ServiceAPI
             if(responseBody != null)
             {
                 //initialise payload with json data parsed
-                loginPayload.id = responseBody.id;
-                loginPayload.username = responseBody.username;
-                loginPayload.password = responseBody.password;
+                loginPayload.Id = responseBody.Id;
+                loginPayload.FirstName = responseBody.FirstName;
+                loginPayload.Password = responseBody.Password;
 
                 return loginPayload;
             } 
@@ -48,35 +49,35 @@ public class ServiceAPI
         }
     }
     //if home team creates new user and sends it to the API, this task will be deleted
-    public async Task<User> RegisterUserTask(string username, string password)
-    {
-        //create payload - User object
-        var loginPayload = new User { username = username, password = password };
+    //public async Task<User> RegisterUserTask(string username, string password)
+    //{
+    //    //create payload - User object
+    //    var loginPayload = new User { username = username, password = password };
 
-        var response = await _httpClient.PostAsJsonAsync("/api/register-user", loginPayload);
+    //    var response = await _httpClient.PostAsJsonAsync("/api/register-user", loginPayload);
 
-        if (response.IsSuccessStatusCode) //check for success response
-        {
-            var responseBody = await response.Content.ReadFromJsonAsync<User>();
-            if (responseBody != null)
-            {
-                //initialise payload with json data parsed
-                loginPayload.id = responseBody.id;
-                loginPayload.username = responseBody.username;
-                loginPayload.password = responseBody.password;
+    //    if (response.IsSuccessStatusCode) //check for success response
+    //    {
+    //        var responseBody = await response.Content.ReadFromJsonAsync<User>();
+    //        if (responseBody != null)
+    //        {
+    //            //initialise payload with json data parsed
+    //            loginPayload.id = responseBody.id;
+    //            loginPayload.username = responseBody.username;
+    //            loginPayload.password = responseBody.password;
 
-                return loginPayload;
-            }
-            else
-            {
-                throw new Exception("Login failed: No user data returned.");
-            }
-        }
-        else
-        {
-            // Return an error if the response is not successful
-            throw new Exception("Login failed: Invalid credentials.");
-        }
-    }
+    //            return loginPayload;
+    //        }
+    //        else
+    //        {
+    //            throw new Exception("Login failed: No user data returned.");
+    //        }
+    //    }
+    //    else
+    //    {
+    //        // Return an error if the response is not successful
+    //        throw new Exception("Login failed: Invalid credentials.");
+    //    }
+    //}
 
 }
